8e00baa9f8adba06cab19c3e3bfd4c87152492b3
arsenyafonin <arseny@softculture.cc>
2017-02-28 21:06:18 +0300

    Update passchecker_readthedocs.js

--- docs/passchecker_readthedocs.js
+++ docs/passchecker_readthedocs.js
@@ -8,6 +8,7 @@ helplist.push("rtd"); /*Арсений*/
 helplist.push("9771002aurov"); /*Витя*/
 helplist.push("2508475"); /*Вася*/
 helplist.push("Y7da2"); /*Настя*/
+helplist.push("kirberezhnov@gmail.com"); /*Кирилл*/
 
 /*Проверка HTML5 Storage*/
 if (typeof(Storage) !== "undefined") {

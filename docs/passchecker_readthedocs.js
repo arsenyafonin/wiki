8739704f34d5ea2bac11ba6ede0e738ec4c61466
Soft Culture <tools@softculture.cc>
2017-03-30 21:15:25 +0300

    Update passchecker_readthedocs.js

--- docs/passchecker_readthedocs.js
+++ docs/passchecker_readthedocs.js
@@ -3,13 +3,13 @@
 var helplist = [];
 
 /*Доступные пароль*/
-helplist.push("master");
 helplist.push("w14february"); /*Арсений*/
 helplist.push("9771002aurov"); /*Витя*/
 helplist.push("2508475"); /*Вася*/
 helplist.push("Y7da2"); /*Настя*/
 helplist.push("kirberezhnov@gmail.com"); /*Кирилл*/
 helplist.push("gogen2709"); /*Руслан*/
+helplist.push("0112358"); /*Сергей*/
 
 /*Проверка HTML5 Storage*/
 if (typeof(Storage) !== "undefined") {
